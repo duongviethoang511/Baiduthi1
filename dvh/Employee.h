386a0e659e9d3789e72b9290dea813b481b2634b
duongviethoang511 <92080405+duongviethoang511@users.noreply.github.com>
2021-12-21 10:49:37 +0700

    Add project files.

---
+++ dvh/Employee.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Employee
+{
+protected:
+	int m_id{ 0 };
+	string m_name;
+	string m_email;
+	int m_age{ 0 };
+
+public:
+
+	Employee() {};
+
+	Employee(int id, string name, int age )
+		: m_id{ id }, m_name{ name }, m_age{ age }
+	{}
+
+	//
+	virtual void show();
+
+	virtual void input();
+
+	
+	string getName();
+
+	int getAge();
+};
